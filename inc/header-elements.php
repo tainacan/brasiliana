742533b7640de0000a1e50304717171df517972f
mateuswetah <mateus.m.luna@gmail.com>
2021-09-20 11:38:38 -0300

    Adds social share icons and some animations.

--- inc/header-elements.php
+++ inc/header-elements.php
@@ -6,15 +6,18 @@
 function brasiliana_add_share_and_back_buttons() {
     ?>
     <div class="brasiliana-header-elements">
-        <button class="brasiliana-header-button brasiliana-header-button--back" onclick="window && window.history && window.history ? window.history.back() : null">
+        <button class="brasiliana-header-button brasiliana-header-button--back" onclick="event.preventDefault(); window && window.history && window.history ? window.history.back() : null">
             <span class="icon">
                 <i class="tainacan-icon tainacan-icon-previous"></i>
             </span>
         </button>
-        <button class="brasiliana-header-button brasiliana-header-button--share">
+        <button class="brasiliana-header-button brasiliana-header-button--share" onclick="event.preventDefault(); document.getElementsByClassName('brasiliana-social-icons')[0].classList.toggle('is-list-open');">
             <span class="icon">
                 <i class="tainacan-icon tainacan-icon-share"></i>
             </span>
+            <div class="brasiliana-social-icons">
+                <?php echo blocksy_get_social_share_box(); ?>
+            </div>
         </button>
     </div>
     <?php
