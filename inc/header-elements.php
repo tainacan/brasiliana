563b5e2eaeac246dc43479b462a5083090ea2a5d
mateuswetah <mateus.m.luna@gmail.com>
2021-09-22 17:45:58 -0300

    Version 0.0.11

--- inc/header-elements.php
+++ inc/header-elements.php
@@ -8,7 +8,7 @@ function brasiliana_add_share_and_back_buttons() {
     <div class="brasiliana-header-elements">
         <button class="brasiliana-header-button brasiliana-header-button--back" onclick="event.preventDefault(); window && window.history && window.history ? window.history.back() : null">
             <span class="icon">
-                <i class="tainacan-icon tainacan-icon-previous"></i>
+                <i class="dashicons dashicons-arrow-left-alt"></i>
             </span>
         </button>
         <button class="brasiliana-header-button brasiliana-header-button--share" onclick="event.preventDefault(); document.getElementsByClassName('brasiliana-social-icons')[0].classList.toggle('is-list-open');">
@@ -22,4 +22,4 @@ function brasiliana_add_share_and_back_buttons() {
     </div>
     <?php
 }
-add_action('blocksy:hero:title:before', 'brasiliana_add_share_and_back_buttons');
+add_action('blocksy:hero:before', 'brasiliana_add_share_and_back_buttons');
