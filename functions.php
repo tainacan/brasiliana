3a2819314ebc0387c7675737c37e36f4d28d0aae
mateuswetah <mateus.m.luna@gmail.com>
2021-09-23 09:39:22 -0300

    Version 0.0.12

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 Theme Name: Brasiliana
 Description: Brasiliana Museus - a child theme of Blocksy
 Author: wetah
-Version: 0.0.11
+Version: 0.0.12
 Text Domain: brasiliana
 */
 
@@ -12,7 +12,7 @@ if (! defined('WP_DEBUG')) {
 }
 
 /** Child Theme version */
-const BRASILIANA_VERSION = '0.0.11';
+const BRASILIANA_VERSION = '0.0.12';
 
 /* Enqueues necessary JS and CSS files */
 add_action( 'wp_enqueue_scripts', function () {
