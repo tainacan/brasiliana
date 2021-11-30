c09a4a7e0af11b5dd9ed3d87528b009c121af47e
mateuswetah <mateus.m.luna@gmail.com>
2021-11-30 16:35:24 -0300

    Updates reference to parent theme and minified css.

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 Theme Name: Brasiliana
 Description: Brasiliana Museus - a child theme of Blocksy
 Author: wetah
-Version: 0.1.6
+Version: 0.1.7
 Text Domain: brasiliana
 */
 
@@ -12,12 +12,12 @@ if (! defined('WP_DEBUG')) {
 }
 
 /** Child Theme version */
-const BRASILIANA_VERSION = '0.1.6';
+const BRASILIANA_VERSION = '0.1.7';
 
 /* Enqueues necessary JS and CSS files */
 add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.min.css' );
-	wp_enqueue_style( 'brasiliana-style', get_stylesheet_uri(), array('parent-style'), BRASILIANA_VERSION );
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'brasiliana-style', get_stylesheet_directory_uri() . '/style.min.css', array('parent-style'), BRASILIANA_VERSION );
 	wp_enqueue_style( 'dashicons' ); // Used in some parts of the theme, even if not logged.
 	wp_enqueue_script( 'brasiliana-search-scripts', get_stylesheet_directory_uri() . '/js/search-scripts.js', array(), BRASILIANA_VERSION );
 });
